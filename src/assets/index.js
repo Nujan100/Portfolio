61d570b610ff949ab4a5683228ba6db24e179fc7
nujan100 <nujan100@gmail.com>
2023-07-04 12:21:41 +0545

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,81 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import OnviroTech from "./company/OnviroTech.png";
+import PCPS from "./company/PCPS.png";
+import laliguras from "./company/laliguras.svg";
+import tesla from "./company/tesla.png";
+import laliguras1 from "./laliguras-1.png";
+import lionsapp from "./lionsapp.jpg";
+import nepaltravel from "./nepaltravel.jpg";
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import britishcollege from "./britishcollege.png";
+import mountsinai from "./mountsinai.png";
+import islington from "./islington.png";
+import LA from "./LA.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  OnviroTech,
+  laliguras,
+  PCPS,
+  laliguras1,
+  lionsapp,
+  nepaltravel,
+  britishcollege,
+  mountsinai,
+  islington,
+  LA,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,307 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  OnviroTech,
+  laliguras,
+  PCPS,
+  laliguras1,
+  threejs,
+  lionsapp,
+  nepaltravel,
+  britishcollege,
+  mountsinai,
+  islington,
+  LA,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Flutter Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Database Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Facuty Member",
+    company_name: "PCPS College",
+    icon: PCPS,
+    iconBg: "#E6DEDD",
+    date: "September 2022 - Present",
+    points: [
+      "PROVIDED INSTRUCTION AND GUIDANCE TO STUDENTS IN THE SUBJECTS OF PYTHON PROGRAMMING, DATABASE DESIGN, AI, AND FLUTTER.",
+      "CONDUCTED TUTORING SESSIONS AND SMALL GROUP ACTIVITIES TO REINFORCE LEARNING OBJECTIVES, HELPING STUDENTS TO COMPLETE THE ASSIGNMENTS.",
+      "ASSISTED IN GRADING ASSIGNMENTS AND PROVIDED CONSTRUCTIVE FEEDBACK TO STUDENTS, FACILITATING THEIR GROWTH AND IMPROVEMENT.",
+    ],
+  },
+  
+  {
+    title: "Front-End & NodeJs Developer (PART TIME)",
+    company_name: "Laliguras",
+    icon: laliguras,
+    iconBg: "#E6DEDD",
+    date: "November 2022 - Present",
+    points: [
+      "Developing and maintaining web applications using HTML, CSS, NodeJS and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Flutter Developer",
+    company_name: "OnviroTech",
+    icon: OnviroTech,
+    iconBg: "#E6DEDD",
+    date: "September 2021 - August 2022",
+    points: [
+      "Developing and maintaining Mobile applications using Flutter.",
+      "Designing UI/UX for the mobile application",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+
+const education = [
+  {
+    title: "MSC INFORMATION AND TECHNOLOGY",
+    company_name: "British College",
+    icon: britishcollege,
+    iconBg: "#E6DEDD",
+    date: "September 2022 - Current",
+    points: [
+      "The MSc Information and Technology is a programme rich in a specialisation, that provides a transformational experience to its students by integrating the technological and business aspects of modern IT.",
+      "In an era where digitalisation is pervasive across all sectors and industries, the course has been designed to address the high demands of both the private and public sector, by filling the current digital skills gap."
+    ],
+  },
+  
+  {
+    title: "BSC (HONS) COMPUTING - First Class",
+    company_name: "Islington College",
+    icon: islington,
+    iconBg: "#E6DEDD",
+    date: "2018 - 2022",
+    points: [
+      "BSc (Hons) Computing, a 3 year IT course in Nepal is a unique blend of practical and theoretical learning that lays primary focus on the techniques, theory and application along with interpersonal, team working and IT skills",
+      "London Metropolitan University authorises and approves Islington College to deliver and assess part or all of the course. This course is also taught and assessed at London Met’s main campus in London, United Kingdom. London Met holds direct responsibility for quality assurance, curriculum content, and the teaching, learning and assessment strategy."
+    ],
+  },
+  {
+    title: "+2 in Science",
+    company_name: "Little Angles College",
+    icon: LA,
+    iconBg: "#E6DEDD",
+    date: "2016 - 2018",
+    points: [
+      "LAC is the strong foundation of the academically reputed institution of Little Angels’ Education Group",
+      "LAC has state-of-the-art infrastructure and modern self-development approaches conducive to the learning and teaching process",
+    ],
+  },
+  {
+    title: "School",
+    company_name: "Mount Sinai English Boarding School",
+    icon: mountsinai,
+    iconBg: "#E6DEDD",
+    date: "2003 - 2016",
+    points: [
+      "Mount Sinai English Boarding School is a School located at M9GM+6H6, Madhyapur Thimi, 44600, NP.",
+      "The business is listed under school category. It has received 5 reviews with an average rating of 5 stars."
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "laliguras",
+    description:
+      "Web-based platform that allows users to search, book, and manage their Treaking Trip.",
+    tags: [
+      {
+        name: "HTMl",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: laliguras1,
+    source_code_link: "https://laliguras.de",
+  },
+  {
+    name: "LionsClub Nepal App",
+    description:
+      "Lions Club Offical Mobile application for Android/IOS.",
+    tags: [
+      {
+        name: "Flutter",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Figma",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Dart",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: lionsapp,
+    source_code_link: "https://play.google.com/store/apps/details?id=com.LionsClubapp.Nepal",
+  },
+  {
+    name: "Nepal Travel",
+    description:
+      "A comprehensive Mobile Application travel booking platform that allows users to book hotels and offers curated recommendations for popular destinations of Nepal.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: nepaltravel,
+    source_code_link: "https://github.com/Nujan100/ReactChatApp",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects, education };
